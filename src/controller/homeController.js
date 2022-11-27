33cec25e2f13ccdbaab8cad87d62dd771222f460
Wy <githubWy@gmail.com>
2022-11-28 01:56:35 +0700

    add unknow 2

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -12,8 +12,15 @@ let getDetailPage = async(req, res) => {
     let user =  await pool.execute('SELECT * FROM `user` WHERE `id` = ?', [userId])
     return res.send(JSON.stringify(user[0]))
 }
+
+let createNewUser = async(req,res) => {
+    console.log('>>>check request', req.body )
+    let {firstName, lastName, email, address} = req.body
+    await pool.execute('insert into user(firstName, lastName, email, address) values(?, ?, ?, ?)',[firstName, lastName, email, address])
+    return res.redirect('/')
+}
         
 
 module.exports = {
-    getHomepage, getDetailPage
+    getHomepage, getDetailPage, createNewUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -7,7 +7,7 @@ let router = express.Router()
 const initWebRouter = (app) => {
     router.get('/', homeController.getHomepage)
     router.get('/detail/user/:id', homeController.getDetailPage)
-
+    router.post('/createNewUser',homeController.createNewUser)
     return app.use('/', router)
 }
 
